931accb09933db42c77f56b7952415d9894050fb
Tuhin Ahmed <mdtuhin.dev@gmail.com>
2023-08-15 18:08:14 +0600

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import "../styles/globals.css";
+
+const clientSideEmotionCache = createCache({ key: "css" });
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
+}
+
+export default MyApp;
